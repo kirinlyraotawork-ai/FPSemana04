d4ca87230c99dcdd5c1e69451728f9c34f6f5f2d
Kirin <kirinlyraota.work@gmail.com>
2025-11-28 16:38:26 +0000

    last push

--- read_json.py
+++ read_json.py
@@ -1,4 +1,31 @@
 import json
-file=open("data.json", "rt")
-first_line=file.readline()
-file.close()
+
+
+def read_json(path):
+    try:
+            f = open(path, "r", encoding="utf-8")
+            content = f.read().strip()
+            f.close()
+    except FileNotFoundError:
+        print("Erro: Ficheiro Não Encontrado!")
+        return
+    if not path.endswith(".json"):
+         print("Erro: Ficheiro Não Contém JSON Válido!")
+         return 
+    if content == "":
+        print("Erro: Ficheiro Vazio!")
+        return
+
+    required = ["nome", "idade", "localização"]
+    if not all(field in content for field in required):
+        print("Erro: Campos Obrigatórios em Falta!")
+        return
+
+    print(content)
+
+
+if __name__ == "__main__":
+    file_path = input("")
+    read_json(file_path)
+    print("Processo Concluído!")
+
